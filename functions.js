688f54156bac93c72fcb80a1d705e2f11d837761
joannapo <43657170+joannapo@users.noreply.github.com>
2022-02-19 12:20:11 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+const sayHello = () => console.log("Hello2");
+
+const add = (a,b) => {
+    return a+b;
+}
+
+module.exports = {
+    sayHello,
+    add
+}
+
+console.log("Plik functions.js działa");
